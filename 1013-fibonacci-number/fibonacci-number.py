51753aae6a8ac3c7632d4a9c4add05554a844ed7
chandrabhanu18 <147488099+chandrabhanu18@users.noreply.github.com>
2026-09-20 14:42:39 +0530

    Time: 46 ms (66.91%) | Memory: 19.3 MB (19.65%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,10 +1,10 @@
 class Solution:
     def fib(self, n: int) -> int:
-        prev1,prev2=1,2
+        prev1,prev2=0,1
         if n<=1:
             return n
-        for i in range(3,n+1):
+        for i in range(2,n+1):
             current=prev1+prev2
             prev1=prev2
             prev2=current
-        return prev1        
+        return prev2       
